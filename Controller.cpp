ee0deb6f51f5f0c7f47db52efc3fa025d9634eb5
Juan Avelar <juan.avelar16@gmail.com>
2018-01-11 01:22:37 +0100

    done with delete bug stuff in bouncing function.

--- Controller.cpp
+++ Controller.cpp
@@ -115,11 +115,16 @@ void Controller::showGraphicOutput(Window *window_foo, vector <GameElement*>* el
 
 //gets input from checkForColission
 void Controller::bounceOnObject(vector <int>* number_of_ball, vector <GameElement*>* Game_elements, vector <MoveableObject*>* Moveable_objects, Window * window_c) {
+	/**vector to state which game elements will be erased after the range based for*/
+	vector <int> later_be_deleted;
 	for (auto c : *number_of_ball) {
 		for (int i = 0; i < signed(Game_elements->size()); i++) {//check for a brick collision
-			if (i != c) {//skips the collision between the ball and the same ball, because it is phisically not possible
+			if (i != c && (*Game_elements)[i] != nullptr && (*Game_elements)[c] != nullptr) {//skips the collision between the ball and the same ball, because it is phisically not possible
 				GameElement::ElementDestroyed Element_destructed = (*Game_elements)[i]->Bounce((*Game_elements)[c]);//the element in c is the ball //Returns true if brick is destroyed
-				if (Element_destructed == GameElement::destroybrick) { Game_elements->erase(Game_elements->begin() + i); }//if brick is destroyed then it gets rid of the vector element
+				if (Element_destructed == GameElement::destroybrick) { 
+					(*Game_elements)[i] = nullptr;
+					later_be_deleted.push_back(i);
+				}//if brick is destroyed then it gets rid of the vector element
 				if (Element_destructed == GameElement::destroyball) {
 					//the size of number_of_ball is defined in bits, so 1 element is 4, 2 is 8...
 					if (number_of_ball->size() < 2) {
@@ -127,24 +132,39 @@ void Controller::bounceOnObject(vector <int>* number_of_ball, vector <GameElemen
 
 						if (lives < 1) {
 							Game_lost = true;
-							Game_elements->erase(Game_elements->begin() + c);
+							(*Game_elements)[c] = nullptr;//Setting it to null ptr, it cannot be erased until the pc is out of the second for loop
+							later_be_deleted.push_back(c);
 							break;
 						}
 						else {
 							Ball* ball = new Ball{ (*window_c), GameElement::small, "pictures/shiny_pinball.png" };
-							(*Game_elements)[c] = ball;
+							(*Game_elements)[c] = ball;//check that all movable objects are first in the game element vector, use: myvector.emplace ( myvector.begin()+ position, newBlabla );
 							(*Moveable_objects)[c] = ball;
 						}
 					}
 					else {
-						Game_elements->erase(Game_elements->begin() + c);
+						(*Game_elements)[c] = nullptr;
+						later_be_deleted.push_back(c);
 						Moveable_objects->erase(Moveable_objects->begin() + c);
+						//delete also place of the number of ball
 					}
 				}
 			}
 		}
 	}
-
+	//erasing elements from vector
+	for (auto to_delete : later_be_deleted) {
+		Game_elements->erase(Game_elements->begin() + to_delete);//erase previously defined vector spaces to be whiped
+		int it = 0;
+		while (it < signed(number_of_ball->size())) {//a while loop is used to compare the numbers inside number_of_ball and all stuff to be erased, if it matches then position is erased from vector.
+			if ((*number_of_ball)[it] == to_delete) {
+				number_of_ball->erase(number_of_ball->begin() + it);
+			}
+			else {
+				it++;
+			}
+		}//erase the position of the balls in the game elements vector if ball is eliminated
+	}
 }
 
 //You must disinherit the window object to the rest of the objects
